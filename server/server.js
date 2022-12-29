1ebd94cf4e62d37fea75c708d84311ee4d71fb4c
joyumoekpo <joyumoekpo1@gmail.com>
2022-12-29 01:30:02 -0600

    Fixed controllers

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/Habits', getHabits);
-app.delete('/api/Habits/:HabitId', deleteHabit);
-app.put('/api/Habits/:HabitId', updateHabit);
-app.post('/api/Habits/', createHabit);
+app.get('/api/habits', getHabits);
+app.delete('/api/habits/:habitId', deleteHabit);
+app.put('/api/habits/:habitId', updateHabit);
+app.post('/api/habits/', createHabit);
 
-app.get('/api/Journals', getJournals);
-app.delete('/api/Habits/:JournalId', deleteJournal);
-app.put('/api/Habits/:JournalId', updateJournal);
-app.post('/api/Journals/', createJournal)
+// app.get('/api/Journals', getJournals);
+// app.delete('/api/Habits/:JournalId', deleteJournal);
+// app.put('/api/Habits/:JournalId', updateJournal);
+// app.post('/api/Journals/', createJournal)
 
 app.listen(4000, () => console.log(`Your server's running on port 4000!`));
