6be5b2da1b955a71eec8be2b2ba96ae91b96c543
joyumoekpo <joyumoekpo1@gmail.com>
2022-12-28 23:03:08 -0600

    Setup server.js and changed main in package.json to the server file

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const {getHabits, deleteHabit, updateHabit, createHabit, getJournals, deleteJournal, updateJournal, createJournal} = require('./controller.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/Habits', getHabits);
+app.delete('/api/Habits/:HabitId', deleteHabit);
+app.put('/api/Habits/:HabitId', updateHabit);
+app.post('/api/Habits/', createHabit);
+
+app.get('/api/Journals', getJournals);
+app.delete('/api/Habits/:JournalId', deleteJournal);
+app.put('/api/Habits/:JournalId', updateJournal);
+app.post('/api/Journals/', createJournal)
+
+app.listen(4000, () => console.log(`Your server's running on port 4000!`));
