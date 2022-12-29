b167db0b108c2a72f111ab57e2d6fbe1af025a6b
joyumoekpo <joyumoekpo1@gmail.com>
2022-12-29 02:33:20 -0600

    Added axios script link

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const {getHabits, deleteHabit, createHabit} = require('./controller.js');
+const {getHabits, createHabit, deleteHabit} = require('./controller.js');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 app.get('/api/habits', getHabits);
-app.delete('/api/habits/:habitId', deleteHabit);
 app.post('/api/habits/', createHabit);
+app.delete('/api/habits/:habitId', deleteHabit);
 
 app.listen(4000, () => console.log(`Your server's running on port 4000!`));
