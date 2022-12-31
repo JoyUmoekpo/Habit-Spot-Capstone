91020ca7ffd72823bcd942fb9ba2c50bcd22de51
joyumoekpo <joyumoekpo1@gmail.com>
2022-12-30 20:09:35 -0600

    Changed background and added journal button

--- public/main.js
+++ public/main.js
@@ -1,18 +1,27 @@
+// const { default: axios } = require("axios");
+
 const habitsContainer = document.querySelector('#habit-container');
 const form = document.querySelector('form');
 const allHabitsBtn = document.querySelector('#all-habits-btn');
 const clearBtn = document.querySelector('#clear-btn');
 
-const baseURL = `http://localhost:4000/api/habits`
+const baseURL = `http://localhost:4000/api/habits`;
 
 const habitsCallback = ({data: habits}) => displayHabits(habits);
+
 const errCallback = err => console.log(err.response.data);
+
 const getAllHabits = () => axios.get(baseURL).then(habitsCallback).catch(errCallback);
+
 const createHabit = body => axios.post(baseURL, body).then(habitsCallback).catch(errCallback);
+
 const deleteHabit = id => axios.delete(`${baseURL}/${id}`).then(habitsCallback).catch(errCallback);
+
 const clearHabits = () => {habitsContainer.innerHTML = ``};
 
-function submitHandler(e) {
+const alert = () => {alert('Hello!')}
+
+const submitHandler = (e) => {
   e.preventDefault() 
 
   let habit = document.querySelector('#habit')
@@ -32,17 +41,18 @@ function submitHandler(e) {
   time.value = ''
 }
 
-function createHabitCard(habit) {
+const createHabitCard = (habit) => {
   const habitCard = document.createElement('div')
   habitCard.classList.add('habit-card')
   habitCard.innerHTML = `<img alt='Image' src=${habit.image} class="habit-cover" />
   <p>${habit.habit}</p>
   <p>${habit.time}</p>
-  <button onclick="deleteHabit(${habit.id})">Delete Habit</button>`
+  <button onclick="deleteHabit(${habit.id})">Delete Habit</button>
+  <button onclick="alert()">Add Journal Entry</button>`
   habitsContainer.appendChild(habitCard)
 }
 
-function displayHabits(arr) {
+const displayHabits = (arr) =>  {
   habitsContainer.innerHTML = ``
   for (let i = 0; i < arr.length; i++) {
       createHabitCard(arr[i])
