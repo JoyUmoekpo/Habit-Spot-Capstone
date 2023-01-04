f66a968dc19ed8d26d969b92914d8da5473f514f
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-04 13:17:49 -0600

    Edited button positioning and styling across pages

--- public/themes.js
+++ public/themes.js
@@ -1,9 +1,13 @@
 let newTheme = document.querySelector("#themes")
 
-const changeTheme = () => {
-  const theme1 = Math.floor(Math.random() * "/assets");
+// const changeTheme = () => {
+//   const theme1 = "background-image-1.jpg";
+//   const theme2 = "background-image-2.jpg";
+//   const theme3 = "background-image-3.jpg";
 
-  document.body.style.backgroundColor = `${theme1}`;
-}
+//   document.body.style.backgroundColor = `${theme1}, ${theme2}, ${theme3}`;
+// }
 
-newTheme.addEventListener("click", changeTheme);
+newTheme.addEventListener("click", () => {
+  document.body.style.backgroundImage = "url('assets/background-image-1.jpg')", "url('assets/background-image-2.jpg')", "url('assets/background-image-3.jpg')";
+});
